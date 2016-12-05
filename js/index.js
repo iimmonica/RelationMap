e4ad94dd2e43a5a767d31a9b05d9c5cf72ff7abb
iimmonica <iimmonica@outlook.com>
2016-12-06 01:37:56 +0800

    方变圆

--- js/index.js
+++ js/index.js
@@ -1,126 +1,118 @@
-var width = 600;
-var height = 600;
-var img_w = 77;
-var img_h = 90;
+$(document).ready(function() {
+	var NODE_RADIUS = 25;
+	var SVG_WIDTH = $("#relation_map").width();
+	var SVG_HEIGHT = $("#relation_map").height();
+	
+	var svg = d3.select("#relation_map");
 
-var svg = d3.select("body").append("svg")
-	.attr("width", width)
-	.attr("height", height);
+	d3.json("data/relation.json", function(error, root) {
 
-d3.json("data/relation.json", function(error, root) {
+		if(error) {
+			return console.log(error);
+		}
+		console.log(root);
 
-	if(error) {
-		return console.log(error);
-	}
-	console.log(root);
+		var force = d3.layout.force()
+			.nodes(root.nodes)
+			.links(root.relations)
+			.size([SVG_WIDTH, SVG_HEIGHT])
+			.linkDistance(200)
+			.charge(-1500)
+			.start();
 
-	var force = d3.layout.force()
-		.nodes(root.nodes)
-		.links(root.edges)
-		.size([width, height])
-		.linkDistance(200)
-		.charge(-1500)
-		.start();
+		var relation_line = svg.selectAll(".line")
+			.data(root.relations)
+			.enter()
+			.append("line")
+			.attr("class", "line");
 
-	var edges_line = svg.selectAll("line")
-		.data(root.edges)
-		.enter()
-		.append("line")
-		.style("stroke", "#ccc")
-		.style("stroke-width", 1);
-
-	var edges_text = svg.selectAll(".linetext")
-		.data(root.edges)
-		.enter()
-		.append("text")
-		.attr("class", "linetext")
-		.text(function(d) {
-			return d.relation;
-		});
-
-	var nodes_circle = svg.selectAll("circle")
-		.data(root.nodes)
-		.enter()
-		.append("circle")
-		.attr("r", 25)
-		.attr("class", "point")
-		.on("mouseover", function(d, i) {
-			//显示连接线上的文字
-			edges_text.style("fill-opacity", function(edge) {
-				if(edge.source === d || edge.target === d) {
-					return 1.0;
-				}
+		var relation_text = svg.selectAll(".line-text")
+			.data(root.relations)
+			.enter()
+			.append("text")
+			.attr("class", "line-text")
+			.text(function(d) {
+				return d.text;
 			});
-		})
-		.on("mouseout", function(d, i) {
-			//隐去连接线上的文字
-			edges_text.style("fill-opacity", function(edge) {
-				if(edge.source === d || edge.target === d) {
-					return 0.0;
-				}
+
+		var node_point = svg.selectAll(".point")
+			.data(root.nodes)
+			.enter()
+			.append("circle")
+			.attr("r", NODE_RADIUS)
+			.attr("class", "point")
+			.on("mouseover", function(d, i) {
+				relation_text.style("fill-opacity", function(edge) {
+					if(edge.source === d || edge.target === d) {
+						return 1.0;
+					}
+				});
+			})
+			.on("mouseout", function(d, i) {
+				relation_text.style("fill-opacity", function(edge) {
+					if(edge.source === d || edge.target === d) {
+						return 0.0;
+					}
+				});
+			})
+			.call(force.drag);
+
+		var text_dx = -20;
+		var text_dy = 20;
+
+		var node_text = svg.selectAll(".point-text")
+			.data(root.nodes)
+			.enter()
+			.append("text")
+			.attr("class", "node-text")
+			.attr("dx", text_dx)
+			.attr("dy", text_dy)
+			.text(function(d) {
+				return d.name;
 			});
-		})
-		.call(force.drag);
 
-	var text_dx = -20;
-	var text_dy = 20;
+		force.on("tick", function() {
 
-	var nodes_text = svg.selectAll(".nodetext")
-		.data(root.nodes)
-		.enter()
-		.append("text")
-		.attr("class", "nodetext")
-		.attr("dx", text_dx)
-		.attr("dy", text_dy)
-		.text(function(d) {
-			return d.name;
-		});
+			root.nodes.forEach(function(d, i) {
+				d.x = d.x - NODE_RADIUS / 2 < 0 ? NODE_RADIUS / 2 : d.x;
+				d.x = d.x + NODE_RADIUS / 2 > SVG_WIDTH ? SVG_WIDTH - NODE_RADIUS / 2 : d.x;
+				d.y = d.y - NODE_RADIUS / 2 < 0 ? NODE_RADIUS / 2 : d.y;
+				d.y = d.y + NODE_RADIUS / 2 + text_dy > SVG_HEIGHT ? SVG_HEIGHT - NODE_RADIUS / 2 - text_dy : d.y;
+			});
 
-	force.on("tick", function() {
+			relation_line.attr("x1", function(d) {
+				return d.source.x;
+			});
+			relation_line.attr("y1", function(d) {
+				return d.source.y;
+			});
+			relation_line.attr("x2", function(d) {
+				return d.target.x;
+			});
+			relation_line.attr("y2", function(d) {
+				return d.target.y;
+			});
 
-		//限制结点的边界
-		root.nodes.forEach(function(d, i) {
-			d.x = d.x - img_w / 2 < 0 ? img_w / 2 : d.x;
-			d.x = d.x + img_w / 2 > width ? width - img_w / 2 : d.x;
-			d.y = d.y - img_h / 2 < 0 ? img_h / 2 : d.y;
-			d.y = d.y + img_h / 2 + text_dy > height ? height - img_h / 2 - text_dy : d.y;
-		});
+			relation_text.attr("x", function(d) {
+				return(d.source.x + d.target.x) / 2;
+			});
+			relation_text.attr("y", function(d) {
+				return(d.source.y + d.target.y) / 2;
+			});
 
-		//更新连接线的位置
-		edges_line.attr("x1", function(d) {
-			return d.source.x;
-		});
-		edges_line.attr("y1", function(d) {
-			return d.source.y;
-		});
-		edges_line.attr("x2", function(d) {
-			return d.target.x;
-		});
-		edges_line.attr("y2", function(d) {
-			return d.target.y;
-		});
+			node_point.attr("cx", function(d) {
+				return d.x - NODE_RADIUS / 2;
+			});
+			node_point.attr("cy", function(d) {
+				return d.y - NODE_RADIUS / 2;
+			});
 
-		//更新连接线上文字的位置
-		edges_text.attr("x", function(d) {
-			return(d.source.x + d.target.x) / 2;
-		});
-		edges_text.attr("y", function(d) {
-			return(d.source.y + d.target.y) / 2;
-		});
-
-		//更新结点图片和文字
-		nodes_circle.attr("cx", function(d) {
-			return d.x - img_w / 2;
-		});
-		nodes_circle.attr("cy", function(d) {
-			return d.y - img_h / 2;
-		});
-
-		nodes_text.attr("x", function(d) {
-			return d.x
-		});
-		nodes_text.attr("y", function(d) {
-			return d.y + img_w / 2;
+			node_text.attr("x", function(d) {
+				return d.x
+			});
+			node_text.attr("y", function(d) {
+				return d.y + NODE_RADIUS / 2;
+			});
 		});
 	});
 });
